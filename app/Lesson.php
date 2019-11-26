8f5e6a8becc94110581fcb71872c2b9c74831df1
ahmadhdr <ahmadhdr.22@gmail.com>
2019-11-26 17:59:43 +0700

    filter pivot

--- app/Lesson.php
+++ app/Lesson.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Lesson extends Model
 {
     public function users(){
-        return $this->belongsToMany('App\User')->withTimeStamps(); //->withPivot('nama_kolom');
+        return $this->belongsToMany('App\User')->withTimeStamps(); //->withPivot('nama_kolom');  // wherePivot('data') single column // wherePivot('data',[''column]) multiple column
     }
 }
